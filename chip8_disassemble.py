ed456b01ef0c3bc747dab6a34b3ac98128cc613b
ASankaran <arnavsankaran@gmail.com>
2016-06-07 11:27:01 -0500

    Mask opcodes and lookup assembly instruction equivalent

--- chip8_disassemble.py
+++ chip8_disassemble.py
@@ -1,6 +1,7 @@
 import sys
 import binascii
 
+from opcode_table import opcodeTable
 
 def main() :
 	if len(sys.argv) < 2:
@@ -12,8 +13,24 @@ def main() :
 		opcode = f.read(2)
 		while opcode:
 			hexOpcode = binascii.hexlify(opcode)
-			print hexOpcode
+			opcodeInstruction = lookupOpcode(int(hexOpcode, 16))
+			print hexOpcode + " : " + opcodeInstruction
 			opcode = f.read(2)
+
+
+def maskOpcode(opcode):
+	if (opcode & 0xF000) in [0x0000, 0xE000, 0xF000]:
+		opcode &= 0xF0FF
+	elif (opcode & 0xF000) in [0x8000]:
+		opcode &= 0xF00F
+	else:
+		opcode &= 0xF000
+	return opcode
+
+def lookupOpcode(opcode):
+	opcode = maskOpcode(opcode)
+	return opcodeTable.get(opcode, "???");
+
 
 if __name__ == '__main__':
 	main()
